be7607b65f0fecdcc64e0e469f6814d41c035a2d
Geoff Massanek <gmassanek@gmail.com>
2015-06-26 18:31:42 -0400

    add revision

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,5 @@
 class StatusController < ApplicationController
   def index
-    render({ json: { status: "OK" } })
+    render({ json: { status: "OK", revision: `cat /var/www/fantasy-celebrity-api/current/REVISION`.try(:chomp) } })
   end
 end
